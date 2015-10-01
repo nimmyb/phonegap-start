d43be9cc99d074ed397bd44d1e877fcd89be5874
nimmyb <nimmyb@but.ae>
2015-10-01 16:07:54 +0400

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -16,34 +16,90 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ var email;
+ var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
-    },
+
+     this.bindEvents();
+   },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+      document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+      app.receivedEvent('deviceready');
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+       // alert(device.uuid);
+       $(".device-id").html(device.uuid);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+       
+       if (window.localStorage.getItem("install") == undefined) {
+         /* run function */
+         $('#demoBox').modal("show"); 
+         var val=window.localStorage.getItem("install");
+         window.localStorage.setItem("install", true);
+       }
+      
+
+       window.plugins.DeviceAccounts.getEmail(function(accounts){
+  // accounts is an array with objects containing name and type attributes
+
+
+  //alert(accounts.length);
+  $(".user-name").html(accounts);
+  
+  Insertdata(accounts);
+      //alert('account registered on this device:'+accounts);
+
+      window.plugins.socialsharing.available(function(isAvailable) {
+  // the boolean is only false on iOS < 6
+  if (isAvailable) {
+
+    // now use any of the share() functions
+  }
+});
+
+  //alert('account registered on this device:', accounts);
+}, function(error){
+  alert('Fail to retrieve accounts, details on exception:', error);
+});
+
+
+
+       var parentElement = document.getElementById(id);
+       var listeningElement = parentElement.querySelector('.listening');
+       var receivedElement = parentElement.querySelector('.received');
+
+       listeningElement.setAttribute('style', 'display:none;');
+       receivedElement.setAttribute('style', 'display:block;');
+
+       console.log('Received Event: ' + id);
+
+
+     }
+   };
+
+   app.initialize();
+   function Insertdata(email)
+   {
+
+
+    var email_id= email;
+    var deviceid= $(".device-id").html();
+
+
+
+    calls();
+  }
